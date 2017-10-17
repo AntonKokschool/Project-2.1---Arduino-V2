7bb9829c7aa753bbf7f9131b7b062bd130b5e9fd
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2017-10-17 11:26:50 +0200

    Added Atmel Studio project files and main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Project-2.1---Arduino.c
+ *
+ * Created: 16-10-2017 15:52:44
+ * Author : Ricardo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
